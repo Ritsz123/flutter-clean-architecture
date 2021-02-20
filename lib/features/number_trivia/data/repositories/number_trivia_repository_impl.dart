0fc1d057112c214ac78823dcca3cdef88db0c445
ritesh <48131636+Ritsz123@users.noreply.github.com>
2021-02-20 17:07:36 +0530

    network info implementation and test

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:clean_flutter/core/error/exceptions.dart';
 import 'package:clean_flutter/core/error/failures.dart';
-import 'package:clean_flutter/core/platform/NetworkInfo.dart';
+import 'package:clean_flutter/core/network/NetworkInfo.dart';
 import 'package:clean_flutter/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:clean_flutter/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:clean_flutter/features/number_trivia/domain/entities/number_trivia.dart';
