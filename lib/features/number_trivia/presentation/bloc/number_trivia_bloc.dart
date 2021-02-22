dc49778e0659561dc7909afdbd641281a1e604f6
ritesh <48131636+Ritsz123@users.noreply.github.com>
2021-02-22 20:10:01 +0530

    BLoC setup

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:clean_flutter/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  NumberTriviaBloc() : super(Empty());
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
